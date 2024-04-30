65c58743d6f46c8f1d337d58c5fbd7bb0941850e
Giulio Neusch-Frediani <giulio@Giulio-M2.local>
2024-04-30 23:12:36 +0200

    fzf and p10k configs

--- .zshrc
+++ .zshrc
@@ -1,34 +1,18 @@
-export PATH=$HOME/bin:/usr/local/bin:$PATH
 export ZSH="$HOME/.oh-my-zsh"
-
-source $ZSH/oh-my-zsh.sh
 
 # Powerlevel 10k custom theme
 ZSH_THEME="powerlevel10k/powerlevel10k"
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
-
-
-plugins=(git, zsh-autosuggestions)
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-# Example aliases
-alias zshconfig="vim ~/.zshrc"
-alias ohmyzsh="vim ~/.oh-my-zsh"
+# Plugins
+plugins=(
+    git
+    zsh-autosuggestions
+    z
+)
+source $ZSH/oh-my-zsh.sh
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# ALIASES
-alias gco="git checkout"
-alias gs="git status"
-alias gp="git pull"
-alias gf="git fetch -p"
-alias cdcore="cd ~/git/autocrane-core/build"
-alias cdhome="cd ~"
-alias cddesktop="cd ~/Desktop
-
-# Import some other scripts"
-source ~/.ffmpeg
+ 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,25 @@
-brew install iterm2
-brew install fzf
-brew install rsync
-
+# Install Oh My Zsh
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
 sleep 1
 
+# Install autosuggestions plugin
 rm -rf ${~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+zsh ~/.fzf/install
+
+# Install powerlevel 10k Theme
 rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Sync everything to home dir + activate.
 rsync --exclude ".git/" \
     --exclude "bootstrap.sh" \
     --exclude "README.md" \
     --exclude ".gitignore" \
+    --exclude "bin" \
     -avh --no-perms . ~;
 source ~/.zshrc;
